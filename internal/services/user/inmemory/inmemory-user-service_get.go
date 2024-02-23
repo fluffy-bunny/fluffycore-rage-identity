a530ddae911bcd5145d77c6fbad410d7a124547f
Herb Stahl <ghstahl@gmail.com>
2024-02-23 13:20:27 -0800

    split apart

---
+++ internal/services/user/inmemory/inmemory-user-service_get.go
@@ -0,0 +1,42 @@
+package inmemory
+
+import (
+	"context"
+
+	proto_oidc_user "github.com/fluffy-bunny/fluffycore-rage-oidc/proto/oidc/user"
+	fluffycore_utils "github.com/fluffy-bunny/fluffycore/utils"
+	status "github.com/gogo/status"
+	zerolog "github.com/rs/zerolog"
+	codes "google.golang.org/grpc/codes"
+)
+
+func (s *service) validateGetUserRequest(request *proto_oidc_user.GetUserRequest) error {
+	if request == nil {
+		return status.Error(codes.InvalidArgument, "request is required")
+	}
+	if fluffycore_utils.IsEmptyOrNil(request.Subject) {
+		return status.Error(codes.InvalidArgument, "request.Subject is required")
+	}
+	return nil
+
+}
+
+func (s *service) GetUser(ctx context.Context, request *proto_oidc_user.GetUserRequest) (*proto_oidc_user.GetUserResponse, error) {
+	log := zerolog.Ctx(ctx).With().Logger()
+	err := s.validateGetUserRequest(request)
+	if err != nil {
+		log.Warn().Err(err).Msg("validateGetUserRequest")
+		return nil, err
+	}
+	//--~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-//
+	s.rwLock.RLock()
+	defer s.rwLock.RUnlock()
+	//--~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-//
+	user, ok := s.userMap[request.Subject]
+	if ok {
+		return &proto_oidc_user.GetUserResponse{
+			User: user,
+		}, nil
+	}
+	return nil, status.Error(codes.NotFound, "User not found")
+}
