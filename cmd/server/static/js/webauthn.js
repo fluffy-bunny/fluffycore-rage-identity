047b18c28e934fbed6d05c972a2ee8620068234a
Herb Stahl <ghstahl@gmail.com>
2024-03-31 11:13:41 -0700

    code cleanup

--- cmd/server/static/js/webauthn.js
+++ cmd/server/static/js/webauthn.js
@@ -1,7 +1,6 @@
-$(document).ready(function () {
+document.addEventListener("DOMContentLoaded", function () {
   if (!window.PublicKeyCredential) {
     alert("Client not capable. Handle error.");
-    return;
   }
 });
 
@@ -17,152 +16,120 @@ function bufferEncode(value) {
     .replace(/=/g, "");
 }
 
-function registerUser(returnUrl) {
-  $.get(
-    "/webauthn/register/begin",
-    null,
-    function (data) {
-      return data;
-    },
-    "json"
-  )
-    .then((beginResponse) => {
-      console.log("beginResponse:", beginResponse);
+async function registerUser(returnUrl) {
+  try {
+    const response = await fetch("/webauthn/register/begin");
+    if (!response.ok) {
+      throw new Error(`Error fetching begin data: ${response.status}`);
+    }
+    const beginResponse = await response.json();
+    console.log("beginResponse:", beginResponse);
 
-      beginResponse.publicKey.challenge = bufferDecode(
-        beginResponse.publicKey.challenge
-      );
-      beginResponse.publicKey.user.id = bufferDecode(
-        beginResponse.publicKey.user.id
-      );
+    beginResponse.publicKey.challenge = bufferDecode(
+      beginResponse.publicKey.challenge
+    );
+    beginResponse.publicKey.user.id = bufferDecode(beginResponse.publicKey.user.id);
 
-      //if (beginResponse.publicKey.excludeCredentials) {
-      //  for (var i = 0; i < beginResponse.publicKey.excludeCredentials.length; i++) {
-      //    beginResponse.publicKey.excludeCredentials[i].id = bufferDecode(beginResponse.publicKey.excludeCredentials[i].id);
-      //  }
-      //}
-
-      return navigator.credentials.create({
-        publicKey: beginResponse.publicKey,
-      });
-    })
-    .then((credentialsResponse) => {
-      console.log("credentialsResponse:", credentialsResponse);
-
-      let attestationObject = credentialsResponse.response.attestationObject;
-      let clientDataJSON = credentialsResponse.response.clientDataJSON;
-      let rawId = credentialsResponse.rawId;
-      let csrf = getCSRF();
-
-      fetch("/webauthn/register/finish", {
-        method: "POST",
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json',
-          'X-Csrf-Token': csrf
-        },
-        body: JSON.stringify({
-          id: credentialsResponse.id,
-          rawId: bufferEncode(rawId),
-          type: credentialsResponse.type,
-          response: {
-            attestationObject: bufferEncode(attestationObject),
-            clientDataJSON: bufferEncode(clientDataJSON),
-          },
-        }),
-      })
-        .then(response => response.json())
-        .then(finishResponse => {
-          console.log("finishResponse:", finishResponse);
-          window.location.href = returnUrl;
-        })
-        .catch(error => {
-          console.error("Error:", error);
-          // Handle errors
-        });
-
-      
-    })
-    .then((success) => {
-      console.log("success:", success);
-      alert("successfully registered !");
-    })
-    .catch((error) => {
-      console.log("failed:", error);
-      alert("occur exception");
+    const credentialsResponse = await navigator.credentials.create({
+      publicKey: beginResponse.publicKey,
     });
+    console.log("credentialsResponse:", credentialsResponse);
+
+    const attestationObject = credentialsResponse.response.attestationObject;
+    const clientDataJSON = credentialsResponse.response.clientDataJSON;
+    const rawId = credentialsResponse.rawId;
+    const csrf = getCSRF();
+
+    const finishResponse = await fetch("/webauthn/register/finish", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        "X-Csrf-Token": csrf,
+      },
+      body: JSON.stringify({
+        id: credentialsResponse.id,
+        rawId: bufferEncode(rawId),
+        type: credentialsResponse.type,
+        response: {
+          attestationObject: bufferEncode(attestationObject),
+          clientDataJSON: bufferEncode(clientDataJSON),
+        },
+      }),
+    });
+
+    if (!finishResponse.ok) {
+      throw new Error(`Error registering user: ${finishResponse.status}`);
+    }
+
+    const data = await finishResponse.json();
+    console.log("finishResponse:", data);
+    window.location.href = returnUrl;
+  } catch (error) {
+    console.error("Error:", error);
+    alert("Registration failed!"); // More specific message based on error
+  }
 }
 
-function LoginUser() {
-  $.get(
-    "/webauthn/login/begin",
-    null,
-    function (data) {
-      return data;
-    },
-    "json"
-  )
-    .then((beginResponse) => {
-      console.log("beginResponse: ", beginResponse);
 
-      beginResponse.publicKey.challenge = bufferDecode(
-        beginResponse.publicKey.challenge
-      );
-      beginResponse.publicKey.allowCredentials.forEach(function (
-        allowCredential
-      ) {
-        allowCredential.id = bufferDecode(allowCredential.id);
-      });
+async function LoginUser() {
+  try {
+    const response = await fetch("/webauthn/login/begin");
+    if (!response.ok) {
+      throw new Error(`Error fetching begin data: ${response.status}`);
+    }
+    const beginResponse = await response.json();
+    console.log("beginResponse: ", beginResponse);
 
-      return navigator.credentials.get({
-        publicKey: beginResponse.publicKey,
-      });
-    })
-    .then((getCredential) => {
-      console.log("getCredential: ", getCredential);
-
-      let authData = getCredential.response.authenticatorData;
-      let clientDataJSON = getCredential.response.clientDataJSON;
-      let rawId = getCredential.rawId;
-      let sig = getCredential.response.signature;
-      let userHandle = getCredential.response.userHandle;
-      let csrf = getCSRF();
-
-      fetch("/webauthn/login/finish", {
-        method: "POST",
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json',
-          'X-Csrf-Token': csrf
-        },
-        body: JSON.stringify({
-          id: getCredential.id,
-          rawId: bufferEncode(rawId),
-          type: getCredential.type,
-          response: {
-            authenticatorData: bufferEncode(authData),
-            clientDataJSON: bufferEncode(clientDataJSON),
-            signature: bufferEncode(sig),
-            userHandle: bufferEncode(userHandle),
-          },
-        }),
-      })
-        .then(response => response.json())
-        .then(finishResponse => {
-          console.log("finishResponse:", finishResponse);
-          window.location.href = finishResponse.redirectUri;
-        })
-        .catch(error => {
-          console.error("Error:", error);
-          // Handle errors
-        });
-    })
-    .then((success) => {
-      alert("successfully logged in " + username + "!");
-      return;
-    })
-    .catch((error) => {
-      console.log(error);
-      alert("failed to login " + username);
+    beginResponse.publicKey.challenge = bufferDecode(
+      beginResponse.publicKey.challenge
+    );
+    beginResponse.publicKey.allowCredentials.forEach((allowCredential) => {
+      allowCredential.id = bufferDecode(allowCredential.id);
     });
+
+    const credential = await navigator.credentials.get({
+      publicKey: beginResponse.publicKey,
+    });
+    console.log("getCredential: ", credential);
+
+    const authData = credential.response.authenticatorData;
+    const clientDataJSON = credential.response.clientDataJSON;
+    const rawId = credential.rawId;
+    const sig = credential.response.signature;
+    const userHandle = credential.response.userHandle || null; // Handle optional userHandle
+    const csrf = getCSRF();
+
+    const finishResponse = await fetch("/webauthn/login/finish", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        "X-Csrf-Token": csrf,
+      },
+      body: JSON.stringify({
+        id: credential.id,
+        rawId: bufferEncode(rawId),
+        type: credential.type,
+        response: {
+          authenticatorData: bufferEncode(authData),
+          clientDataJSON: bufferEncode(clientDataJSON),
+          signature: bufferEncode(sig),
+          userHandle: userHandle ? bufferEncode(userHandle) : undefined, // Handle optional userHandle
+        },
+      }),
+    });
+
+    if (!finishResponse.ok) {
+      throw new Error(`Error logging in user: ${finishResponse.status}`);
+    }
+
+    const data = await finishResponse.json();
+    console.log("finishResponse:", data);
+    window.location.href = data.redirectUri;
+  } catch (error) {
+    console.error("Error:", error);
+    alert("Login failed!"); // More specific message based on error
+  }
 }
+
