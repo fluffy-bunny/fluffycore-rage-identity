b8e7b51e11d66723cfe83ab93c9512c00c70c2d7
rarshynnyk <ruslanarshynnyk@gmail.com>
2024-08-06 22:48:11 +0200

    feat: adjust build

--- client/account-management/src/api/index.ts
+++ client/account-management/src/api/index.ts
@@ -1,5 +1,6 @@
 import { AxiosError } from 'axios';
 
+import { getCSRF } from '../utils/cookies';
 import { Api } from './Api';
 
 export const {
@@ -11,6 +12,16 @@ export const {
   baseURL: process.env.REACT_APP_API_URL,
   withCredentials: true,
   withXSRFToken: true,
+});
+
+apiInstance.interceptors.request.use((config) => {
+  const xcrfToken = getCSRF();
+
+  if (xcrfToken) {
+    config.headers['X-Csrf-Token'] = xcrfToken;
+  }
+
+  return config;
 });
 
 export const isApiError = (error: any): error is AxiosError => {
