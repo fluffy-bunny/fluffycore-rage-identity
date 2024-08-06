f7732a8fdd8667bb9d18ff28d44e38fe2a5e8228
rarshynnyk <ruslanarshynnyk@gmail.com>
2024-08-06 21:41:48 +0200

    feat: adjust build

--- client/account-management/config-overrides.js
+++ client/account-management/config-overrides.js
@@ -1,9 +1,14 @@
-const path = require("path");
+const path = require('path');
 
-module.exports = function override(config, env) {
-  if (env === "production") {
-    config.output.filename = "static/js/main.js";
-  }
-
-  return config;
+module.exports = {
+  webpack: function override(config, env) {
+    if (env === 'production') {
+      config.output.filename = 'static/js/main.js';
+    }
+    return config;
+  },
+  paths: function (paths) {
+    paths.appBuild = path.resolve(__dirname, 'account-management');
+    return paths;
+  },
 };
