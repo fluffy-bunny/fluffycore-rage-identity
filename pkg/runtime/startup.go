d3fccf2410f3dbe0a7420222c201ffda2be24fae
Herb Stahl <ghstahl@gmail.com>
2024-10-11 16:00:18 -0700

    cleanup

--- pkg/runtime/startup.go
+++ pkg/runtime/startup.go
@@ -9,7 +9,6 @@ import (
 	di "github.com/fluffy-bunny/fluffy-dozm-di"
 	contracts_config "github.com/fluffy-bunny/fluffycore-rage-identity/pkg/contracts/config"
 	oidcserver "github.com/fluffy-bunny/fluffycore-rage-identity/pkg/oidcserver"
-	oidcuiserver "github.com/fluffy-bunny/fluffycore-rage-identity/pkg/oidcuiserver"
 	services "github.com/fluffy-bunny/fluffycore-rage-identity/pkg/services"
 	services_health "github.com/fluffy-bunny/fluffycore-rage-identity/pkg/services/health"
 	pkg_types "github.com/fluffy-bunny/fluffycore-rage-identity/pkg/types"
@@ -49,9 +48,6 @@ type (
 
 		oidcserverFuture  async.Future[*fluffycore_async.AsyncResponse]
 		oidcserverRuntime *core_echo_runtime.Runtime
-
-		oidcuiserverFuture  async.Future[*fluffycore_async.AsyncResponse]
-		oidcuiserverRuntime *core_echo_runtime.Runtime
 
 		ext pkg_types.ConfigureServices
 	}
@@ -251,25 +247,6 @@ func (s *startup) OnPreServerStartup(ctx context.Context) error {
 		}
 	})
 
-	s.oidcuiserverRuntime = core_echo_runtime.New(oidcuiserver.NewStartup(
-		oidcuiserver.WithConfigureServices(func(ctx context.Context, builder di.ContainerBuilder) {
-
-		}),
-	))
-	s.oidcuiserverFuture = fluffycore_async.ExecuteWithPromiseAsync(func(promise async.Promise[*fluffycore_async.AsyncResponse]) {
-		var err error
-		defer func() {
-			promise.Success(&fluffycore_async.AsyncResponse{
-				Message: "End Serve - echoServer",
-				Error:   err,
-			})
-		}()
-		log.Info().Msg("echoServer starting up")
-		err = s.oidcuiserverRuntime.Run()
-		if err != nil {
-			log.Error().Err(err).Msg("failed to start server")
-		}
-	})
 	return nil
 }
 
