9e4ae92a1eaf339d1c1cf9184924c6bc59e972cc
Herb Stahl <ghstahl@gmail.com>
2024-12-27 07:40:18 -0500

    latest deps and docker fixes

--- cmd/server/static/blazor/management/wwwroot/service-worker.js
+++ cmd/server/static/blazor/management/wwwroot/service-worker.js
@@ -1,4 +1,4 @@
-/* Manifest version: 0n/LYGft */
+/* Manifest version: vEHbMgHe */
 // Caution! Be sure you understand the caveats before publishing an application with
 // offline support. See https://aka.ms/blazor-offline-considerations
 
