1233f2c17f9593037c4669c2de7b9a514fc9c073
Herb Stahl <ghstahl@gmail.com>
2025-10-25 07:28:46 -0700

    added disable social login to manifest.go

--- pkg/models/api/manifest/manifest.go
+++ pkg/models/api/manifest/manifest.go
@@ -11,6 +11,7 @@ type (
 		LandingPage                 *LandingPage `json:"landing_page"`
 		DevelopmentMode             bool         `json:"development_mode"`
 		DisableLocalAccountCreation bool         `json:"disable_local_account_creation"`
+		DisableSocialAccounts       bool         `json:"disable_social_accounts"`
 	}
 	LandingPage struct {
 		Page Page `json:"page"`
