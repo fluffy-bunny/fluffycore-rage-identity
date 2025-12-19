10cf009cc52de61b608efd4c6626cb2ce52e3e12
Herb Stahl <ghstahl@gmail.com>
2025-12-19 07:07:57 -0800

    rename and fix

--- example/go-app/management/web/build_version.js
+++ example/go-app/management/web/build_version.js
@@ -1,8 +1,8 @@
 // Build version information
 // This file is auto-generated during the build process
 window.BUILD_VERSION = window.BUILD_VERSION || {
-  version: '1766153864',
-  buildTime: '2025-12-19_14:17:44',
-  gitCommit: '379deb2',
+  version: '1766156627',
+  buildTime: '2025-12-19_15:03:47',
+  gitCommit: '93823ae',
   gitBranch: 'main'
 };
--- example/go-app/oidc-login/web/build_version.js
+++ example/go-app/oidc-login/web/build_version.js
@@ -1,8 +1,8 @@
 // Build version information
 // This file is auto-generated during the build process
 window.BUILD_VERSION = window.BUILD_VERSION || {
-  version: '1766153874',
-  buildTime: '2025-12-19_14:17:55',
-  gitCommit: '379deb2',
+  version: '1766156639',
+  buildTime: '2025-12-19_15:03:59',
+  gitCommit: '93823ae',
   gitBranch: 'main'
 };
